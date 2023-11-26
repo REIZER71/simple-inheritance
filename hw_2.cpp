b37387b5001c5a015243b3573c452e9cfe1abf55
REIZER71 <145429747+REIZER71@users.noreply.github.com>
2023-11-26 13:04:44 +1000

    шаг 2
    Добавил свойства и конструктор

--- hw_2.cpp
+++ hw_2.cpp
@@ -4,12 +4,32 @@ using namespace std;
 using namespace std;
 class Human
 {
-	
+public:
+	unsigned int age;
+	string name;
+	bool sex;
+	Human (unsigned int Age, string Name, bool Sex)
+	{
+		age = Age;
+		name = Name;
+		if (Sex ==0)
+		{
+			sex = Sex;
+			cout << "Создана женщина по имени " <<name<< endl;
+		}
+		else if (Sex == 1)
+		{
+			sex = Sex;
+			cout << "Создан мужчина по имени " <<name<< endl;
+		}
+	}
 };
 
 int main()
 {
-	Human human1;
-	Human human2;
+
+	setlocale(0, "");
+	Human human1 (20, "Федор", 1);
+	Human human2 (19, "Маргарита", 0);
 	return 0;
 }
