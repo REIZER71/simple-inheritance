93f721a30b39efcafb27e7b33d79475526258abc
REIZER71 <145429747+REIZER71@users.noreply.github.com>
2023-11-26 13:27:15 +1000

    шаг 3
    сеттеры и геттеры

--- hw_2.cpp
+++ hw_2.cpp
@@ -4,10 +4,11 @@ using namespace std;
 using namespace std;
 class Human
 {
-public:
+private:
 	unsigned int age;
 	string name;
 	bool sex;
+public:
 	Human (unsigned int Age, string Name, bool Sex)
 	{
 		age = Age;
@@ -23,6 +24,14 @@ public:
 			cout << "Создан мужчина по имени " <<name<< endl;
 		}
 	}
+	//getters
+	string getName() const { return name; }
+	int getAge() const { return age; }
+	bool getSex() const { return sex; }
+	//setters
+	void setName(string some_name) { name = some_name;}
+	void setAge(int some_age) { age = some_age; }
+	void setSex(bool some_sex) { sex = some_sex; }
 };
 
 int main()
